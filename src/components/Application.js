f1b040b88f82740887205046270c7cf9b561151d
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2022-08-15 16:32:51 -0600

    Remove hardcoded days data since properly grabbing info from api/days and combine state

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,37 +5,12 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-// const [days, setDays] = useState([]);
-// useEffect(() => {
-//   const daysDataURL = `http:localhost:8001/api/days`;
-//   axios.get(daysDataURL).then(response => {
-//     setDays(...[response.data]);
-//   });
-// }, [])
-
 const appointments = {
-  "1": {
+  1: {
     id: 1,
     time: "12pm",
   },
-  "2": {
+  2: {
     id: 2,
     time: "1pm",
     interview: {
@@ -44,14 +19,14 @@ const appointments = {
         id: 3,
         name: "Sylvia Palmer",
         avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
+      },
+    },
   },
-  "3": {
+  3: {
     id: 3,
     time: "2pm",
   },
-  "4": {
+  4: {
     id: 4,
     time: "3pm",
     interview: {
@@ -60,26 +35,35 @@ const appointments = {
         id: 4,
         name: "Cohana Roy",
         avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
+      },
+    },
   },
-  "5": {
+  5: {
     id: 5,
     time: "4pm",
-  }
+  },
 };
 
-
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    days:"Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}))
+
+  
+  useEffect(() => {
+    const daysDataURL = `/api/days`;
+    axios.get(daysDataURL).then((response) => {
+      setDays(response.data);
+    });
+  }, []);
+  
   const schedule = Object.values(appointments).map((appointment) => {
-    return (
-      <Appointment
-        key={appointment.id}
-        {...appointment}
-      />
-    )
-  })
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -90,11 +74,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
-          />
+          <DayList days={state.days} value={state.day} onChange={setDays} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
