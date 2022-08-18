dd21035ee49348e0040cc491624d75651d4a072c
SJ-WJ <sirajwahaj.ibrahim@gmail.com>
2022-08-17 19:55:36 -0600

    Validate input on submit in the Form and Improve the quality of the tests for the Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -13,6 +13,19 @@ export default function Form(props) {
   const cancel = () => {
     props.onCancel(reset())
   }
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
+  const [error, setError] = useState("");
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -24,7 +37,9 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => {setStudent(event.target.value)}}
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
         interviewers={props.interviewers}
